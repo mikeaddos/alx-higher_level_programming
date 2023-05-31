dba1544f1285f5ec2d6dedaf628af2b6d5bb9699
root <mikeaddos@yahoo.com>
2023-05-31 01:20:42 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Define a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """Represent a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
+        """Initialize a new square
 
         Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
+            size (int): size of the new square
+            position (int, int): position of the new square
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Get/set the current size of the square"""
         return (self.__size)
 
     @size.setter
@@ -30,15 +30,15 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
+        """Get/set the current position of the square"""
         return (self.__position)
 
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(number, int) for number in value) or
-                not all(number >= 0 for number in value)):
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
