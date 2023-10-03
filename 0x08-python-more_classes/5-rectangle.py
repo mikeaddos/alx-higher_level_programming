d237101878b928e46feab16b1b7111bd5614d980
mikeaddos <mikeaddos@yahoo.com>
2023-10-03 03:11:50 +0000

    defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         """ return a string representation of the rectangle
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         """Print the message when an instance of Rectangle is deleted
         """
